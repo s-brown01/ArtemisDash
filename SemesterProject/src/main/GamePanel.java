769d3a95531b3b38e458eb0b4db8feb758325ae5
John <jbotonakis@carroll.edu>
2024-02-26 17:08:22 -0700

    Refreshed GamePanel Class
    Removed references to Rectangle test
    Now, able to draw player sprite to screen and enlarge it

--- SemesterProject/src/main/GamePanel.java
+++ SemesterProject/src/main/GamePanel.java
@@ -1,10 +1,12 @@
 package main;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.KeyListener;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
@@ -12,20 +14,37 @@ import inputs.MouseInputs;
 
 public class GamePanel extends JPanel{
 	
-	private static final long serialVersionUID = 1L;
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private float xDir = 1f, yDir = 1f;
-	private Color color = new Color(150,20,90);
-	private Random random = new Random();
-	
+	private BufferedImage img;
+	private String image = "player_sprites.png";
 	public GamePanel() {
-		mouseInputs = new MouseInputs(this);
+		mouseInputs = new MouseInputs(this);//Forwards all mouse listener events to input class
 		addKeyListener(new KeyboardInputs(this));//Forwards all key listener events to input class
+		
+		importImg();
+		
+		setPanelSize();
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
 	
+	public void importImg() {
+		InputStream is = getClass().getClassLoader().getResourceAsStream(image);
+		
+			try {
+				img = ImageIO.read(is);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280,800);
+		setPreferredSize(size);
+	}
+
 	public void changeXDelta(int value) {
 		this.xDelta += value;
 	}
@@ -41,37 +60,10 @@ public class GamePanel extends JPanel{
 	//graphics allows us to draw and redraw inside panel
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);//Uses JPanel's own paint method
-		g.setColor(color);
-
-		g.fillRect((int)xDelta,(int) yDelta, 200, 50);
-		updateRectangle();
 		
-		
-		
-		
-		g.drawRect((int)xDelta, (int)yDelta, 200, 50);//draws rectangle for testing
-		
-	}
-//Anything regarding a rectangle is to draw SOMETHING to the screen so I'm sure it's working properly
-	private void updateRectangle() {
-		// TODO Auto-generated method stub
-		xDelta += xDir;
-		if(xDelta > 200 || xDelta < 0) {
-			xDir *=-1;
-			color = getRandColor();
-		}
-		yDelta +=yDir;
-		if(yDelta > 330 || yDelta < 0) {
-			yDir *=-1;
-			color = getRandColor();
-		}
-	}
-//Generates random color for DVD Logo Rectangle
-	private Color getRandColor() {
-		// TODO Auto-generated method stub
-		int r = random.nextInt(255);
-		int g = random.nextInt(255);
-		int b = random.nextInt(255);
-		return new Color(r,g,b);
+		g.drawImage(img.getSubimage(100, 70, 64, 60), 0, 0, 120,120, null);
+		//100 px offset from top left corner, 70px offset from top
+		//Width is ~64, Height ~60
+		//Doubled X and Y size 
 	}
 }
