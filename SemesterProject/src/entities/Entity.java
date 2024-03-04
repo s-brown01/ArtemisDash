848b96df4e70922aab96e112f8bf213b825e2445
s-brown01 <sbrown01@carroll.edu>
2024-03-03 22:32:13 -0700

    Made the Entity class into an abstract class (there should never be an Entity object) and then had Player extend Entity. Also, added some basic properties to player

--- SemesterProject/src/entities/Entity.java
+++ SemesterProject/src/entities/Entity.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class Entity {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+
+abstract class Entity {
+	protected float x, y, width, height;
+	protected final Rectangle2D.Float hitbox;
+	
+	public Entity(float x, float y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		hitbox = new Rectangle2D.Float(x, y, width, height);
+		
+	}
+	
+	abstract protected void update();
+	abstract protected void draw(Graphics g);
+	
+	protected void drawHitbox(Graphics g, int xlvlOffset) {
+		g.setColor(Color.pink);
+		g.drawRect((int)hitbox.x - xlvlOffset, (int)hitbox.y, (int)hitbox.width, (int)hitbox.height);
+		
+	}
+	
+	public Rectangle2D  getHitbox() {
+		return hitbox;
+	}
+	
 
 }
--- SemesterProject/src/entities/Player.java
+++ SemesterProject/src/entities/Player.java
@@ -1,5 +1,39 @@
 package entities;
 
-public class Player {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import static utils.Constants.PlayerConstants.*;
+
+public class Player extends Entity {
+	
+	private BufferedImage[][] imgs;
+	private int aniIndex, aniTick;
+	private int aniSpeed = 120 / 10; // 120 frames per second / 10 animations per second
+	private int playerAction = IDLE;
+	private boolean left, right, up, down;
+	
+	private int maxHealth = 2;
+	private int currentHealth = maxHealth;
+
+	public Player(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void draw(Graphics g) {
+		drawHitbox(g, 0);
+	}
+	
+	public int getHealth() {
+		return currentHealth;
+	}
 
 }
