5e08851289b780e58c25cbdc227f29810afcc3cf
s-brown01 <sbrown01@carroll.edu>
2024-03-04 12:21:33 -0700

    Added some example code into the Overworld class

--- SemesterProject/src/states/Overworld.java
+++ SemesterProject/src/states/Overworld.java
@@ -1,16 +1,19 @@
 package states;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import main.Game;
 
 public class Overworld extends State implements StateMethods {
+	
+	private BufferedImage background;
 
 	public Overworld(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -21,8 +24,12 @@ public class Overworld extends State implements StateMethods {
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		g.setColor(new Color(175, 175, 200, 200));
+		g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
+		g.setColor(Color.black);
+		g.drawRect(100, 150, 50, 50); // example square 
+		g.drawString("OVERWORLD", Game.GAME_WIDTH / 2 - 15, 20);
+		g.drawString("Click anywhere to continue to game", Game.GAME_WIDTH - 100, Game.GAME_HEIGHT / 2);
 	}
 
 	@Override
