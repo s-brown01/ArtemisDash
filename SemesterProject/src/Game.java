fe11ca5f38adf179dd0bc1ed843ccc8623842ca0
John <jbotonakis@carroll.edu>
2024-03-03 20:39:00 -0700

    UPS and Animation Transitions
    Added Ability to run left and right with animations and "A" and "D"
    Added UPS - Updates per Second will allow us to not only keep the frame rate at a specified cap, but also allow us to make up for any lost time in between updates

---
+++ SemesterProject/src/Game.java
@@ -0,0 +1,93 @@
+package main;
+
+import states.Playing;
+import states.Menu;
+
+public class Game implements Runnable {
+	
+	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+	private Thread gameThread;
+	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
+	
+	private Playing playing;
+	private Menu menu;
+	
+	// Main Game Constructor
+	public Game() {
+		initClasses();
+		gamePanel = new GamePanel(this);
+		gameWindow = new GameWindow(gamePanel);
+		gamePanel.requestFocus();
+		startGameLoop();
+
+	}
+	
+	private void initClasses() {
+		menu = new Menu(this);
+		playing = new Playing(this);
+	}
+	
+	
+	//Begins main Loop on a seperate thread
+	private void startGameLoop() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	public void update() {
+		gamePanel.updateGame();
+	}
+	@Override
+	public void run() {
+		//FPS Counter - [Don't change, will condense this next commit]
+		double timePerFrame = 1000000000.0/FPS_SET; //A billion seconds
+		double timePerUpdate = 1000000000.0/UPS_SET;//
+		
+		long previousTime = System.nanoTime();
+		
+		int frames = 0;
+		int updates = 0;
+		long lastCheck = System.currentTimeMillis();
+		
+		double deltaU = 0;
+		double deltaF = 0; //deltaFrames
+		
+		while(true) {
+			long currentTime = System.nanoTime();
+			
+			deltaU +=(currentTime - previousTime)/timePerUpdate;
+			deltaF +=(currentTime - previousTime)/timePerFrame;
+			previousTime = currentTime;
+			
+			if (deltaU >=1) {
+				update();
+				updates++;
+				deltaU--;
+			}
+			
+			if(deltaF >=1) {
+				gamePanel.repaint();
+				frames++;
+				deltaF --;
+			}
+			
+			if (System.currentTimeMillis() - lastCheck >=1000) {
+				lastCheck = System.currentTimeMillis();
+				System.out.println("FPS: " + frames + " | UPS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	
+
+	public Playing getPlaying() {
+		return playing;
+	}
+	
+	public Menu getMenu() {
+		return menu;
+	}
+}
