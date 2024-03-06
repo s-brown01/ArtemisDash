21b9f80a1d41b4845b577cb892c8c3005b031fa2
s-brown01 <sbrown01@carroll.edu>
2024-03-06 10:26:56 -0700

    Fixed a typo in Overworld class

--- SemesterProject/src/states/Overworld.java
+++ SemesterProject/src/states/Overworld.java
@@ -29,7 +29,7 @@ public class Overworld extends State implements StateMethods {
         g.setFont(boldFont);
         g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
         g.setColor(Color.black);
-        g.drawString("OVERWORLD", Game.GAME_WIDTH / 2 - 30, 3000);
+        g.drawString("OVERWORLD", Game.GAME_WIDTH / 2 - 30, 300);
         g.drawString("Click anywhere to continue to game", Game.GAME_WIDTH / 2 - 100,
                             Game.GAME_HEIGHT / 2);
     }
