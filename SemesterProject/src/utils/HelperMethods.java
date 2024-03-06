1095ec28523823c510b2994b28dee13a7f0fb2d8
s-brown01 <sbrown01@carroll.edu>
2024-03-06 11:14:54 -0700

    Added temporary InBounds method

--- SemesterProject/src/utils/HelperMethods.java
+++ SemesterProject/src/utils/HelperMethods.java
@@ -1,6 +1,21 @@
 
 package utils;
 
+import java.awt.geom.Rectangle2D;
+
+import main.Game;
+
 public class HelperMethods {
+    
+    /**
+     * THIS IS A TEMPORARY METHOD, used to determine if a hitbox is within the screen
+     * 
+     * @param hitbox
+     * @return
+     */
+    public static boolean InBounds(Rectangle2D.Float hitbox) {
+        return (hitbox.x >= 0 && hitbox.x + hitbox.width <= Game.GAME_WIDTH &&
+                            hitbox.y >= 0 && hitbox.y + hitbox.height <= Game.GAME_HEIGHT);
+    }
 
 }
