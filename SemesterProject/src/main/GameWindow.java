be691e8272cff857642b7914f9f1e50706bdd2ba
s-brown01 <sbrown01@carroll.edu>
2024-03-11 17:47:38 -0600

    Added a JMenuBar, no actions associated with it

--- SemesterProject/src/main/GameWindow.java
+++ SemesterProject/src/main/GameWindow.java
@@ -2,16 +2,37 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 
 public class GameWindow {
 
     private JFrame gameFrame;
+    private JMenuBar menuBar; 
+    private JMenu menu;
+    private JMenuItem m1, m2, m3;
 
     // Game Window Constructor
     public GameWindow(GamePanel gamePanel) {
 	gameFrame = new JFrame();
+	
+	menuBar = new JMenuBar();
+	menu = new JMenu("Menu");
+    m1 = new JMenuItem("Pause");
+    m2 = new JMenuItem("End Level");
+    m3 = new JMenuItem("Quit Game");
+    
+    menu.add(m1);
+    menu.add(m2);
+    menu.add(m3);
+    menuBar.add(menu);
+    
+    gameFrame.setJMenuBar(menuBar);
+    
 
 	gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	gameFrame.add(gamePanel);
