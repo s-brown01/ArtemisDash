d2874896fc1c8579272ecc2176f5a5f34d8f057d
s-brown01 <sbrown01@carroll.edu>
2024-03-12 23:34:12 -0600

    Created framework for Skeleton class and brought in temporary walk animation

---
+++ SemesterProject/src/entities/Skeleton.java
@@ -0,0 +1,25 @@
+package entities;
+
+import java.awt.Graphics;
+import static utils.Constants.EnemyConstants.*;
+
+public class Skeleton extends Enemy {
+
+    public Skeleton(float x, float y, int width, int height) {
+        super(x, y, width, height, SKELETON);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
