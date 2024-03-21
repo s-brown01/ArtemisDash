ff12da89b5097bd9de78b1ca17c95827a5cfd0c2
s-brown01 <sbrown01@carroll.edu>
2024-03-21 13:09:32 -0600

    Added a background to the Overworld

--- SemesterProject/src/utils/LoadSave.java
+++ SemesterProject/src/utils/LoadSave.java
@@ -20,8 +20,10 @@ public class LoadSave {
     public static final String LEVEL_SPRITES = "/World3T.png";
     public static final String LEVEL1_DATA = "/level_one_data.png";
     public static final String SKELETON_SPRITES = "/Skeleton.png";
-
+    public static final String OVERWORLD_BG = "/Overworld-Map.png";
+    
     public static final String SKELETON_WALK = "/skeleton/Skeleton_Walk.png";
+
 
     /**
      * Returns the specified sprite atlas for use in drawing the correct image to the screen
