d2c47eb80ab9db786b0b3f2e1bf95fd700782660
s-brown01 <sbrown01@carroll.edu>
2024-03-26 16:43:30 -0600

    Removed unnecessary comments

--- SemesterProject/src/states/GameStates.java
+++ SemesterProject/src/states/GameStates.java
@@ -11,16 +11,7 @@ public enum GameStates {
 
     PLAYING, MENU, OVERWORLD, OPTIONS, QUIT;
 
-    // TESTING: Start on PLAYING for testing of that state
-//     public static GameStates state = PLAYING;
-
     // every game should start on the menu
     public static GameStates state = MENU;
-
-    // TESTING: Start on OVERWORLD for testing of that state
-    // public static GameStates state = OVERWORLD;
-
-    // TESTING: Start on OPTIONS for testing of that state
-    // public static GameStates state = OPTIONS;
 
 }
