dd752c7a5237e3771c276e54c0adf2039e70dfd5
s-brown01 <sbrown01@carroll.edu>
2024-04-21 15:11:25 -0600

    Fixed typo

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,7 +1,7 @@
 package main;
 
 /**
- * This main class acts as a singluar point to branch off from, when starting the game
+ * This main class acts as a singular point to branch off from, when starting the game
  * 
  * @author John Botonakis and Sean-Paul Brown
  */
